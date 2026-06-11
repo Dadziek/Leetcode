fafd1409ffae30519923085cda69602469900c25
Marcel <122309581+Dadziek@users.noreply.github.com>
2026-06-12 00:08:01 +0200

    Time: 0 ms (100.00%) | Memory: 8.5 MB (22.03%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -0,0 +1,22 @@
+
+
+int fib(int n){
+    if(n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    
+    int lastLast = 0;
+    int last = 1;
+    int current = 0;
+
+    for(int i = 2; i <= n; i++){
+        current = last + lastLast;
+        lastLast = last;
+        last = current;
+    }
+
+    return current;
+}
