28aad85ca820e8c71ea5c70469fbb46f5e93106a
Marcel <122309581+Dadziek@users.noreply.github.com>
2026-07-20 11:45:25 +0200

    Time: 7 ms (5.94%) | Memory: 19.2 MB (78.99%) - LeetSync

---
+++ 968-beautiful-array/beautiful-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def beautifulArray(self, n: int) -> List[int]:
+        if n == 1:
+            return [1]
+
+        odd = self.beautifulArray((n+1) // 2)
+        even = self.beautifulArray(n // 2)
+
+        return [2 * x - 1 for x in odd] + [2 * x for x in even]
